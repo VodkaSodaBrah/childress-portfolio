73297a12b14079cfba662ce87f9f615b3823e7bc
VodkaSodaBrah <mchildress@me.com>
2022-02-13 16:02:51 -0700

    Initial Deployment Commit

---
+++ src/components/pages/about.js
@@ -0,0 +1,43 @@
+import React from "react";
+import profilePicture from "../../../static/assets/images/bio/Me.jpg";
+
+export default function () {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem,
+        ducimus in repellat nostrum voluptatum ipsum accusamus illo veritatis
+        recusandae ex similique voluptas, quia et, sunt blanditiis aspernatur
+        quam suscipit aliquid. Illum consequatur odit atque molestiae officiis
+        sed, eos perspiciatis ducimus repudiandae est veritatis esse provident
+        iste similique minima optio rerum tenetur qui beatae ipsum, possimus
+        dolor laboriosam. Quasi, dignissimos nihil? Culpa ipsa placeat excepturi
+        fugiat nihil harum voluptatum soluta asperiores illum reiciendis tempora
+        maxime ratione consectetur quidem, iusto, omnis magni minima tempore
+        iste delectus commodi ab nisi repellat vel. Nobis? Doloribus quidem
+        quasi, distinctio illo libero excepturi ad molestias iure voluptates.
+        Itaque aperiam, adipisci voluptas ullam harum eius atque excepturi
+        deleniti molestias libero iusto! Ratione facere quaerat harum
+        consectetur soluta. Deleniti ab veniam sapiente perferendis architecto,
+        est dolore, quas ducimus qui ut sint id rem praesentium voluptates totam
+        facilis aperiam, quae aliquid labore? Quos mollitia placeat expedita
+        totam voluptatum eligendi. Ipsa nemo aut dignissimos veritatis,
+        doloremque pariatur aspernatur deserunt perferendis natus totam laborum
+        corporis architecto iure esse, ea placeat dicta magnam cupiditate optio
+        nisi ex possimus incidunt! Doloremque, suscipit quia? Tempora enim
+        voluptatum deserunt ab quam, placeat dolorum provident accusamus
+        asperiores iste voluptatem porro vitae harum nulla, sunt quidem, autem
+        deleniti? Doloribus magnam minus voluptatem voluptas eveniet,
+        consequuntur officia tempore.
+      </div>
+    </div>
+  );
+}
